bd51ea3fde35b20b57389aeab67ddb2c023a3cb8
Ultra Dabkunk <narabodeechaengphrai@gmail.com>
2025-12-23 16:04:02 +0700

    Fix where db.php got sent

--- config/db.php
+++ config/db.php
@@ -1,23 +1,17 @@
 <?php 
 
-//config/db.php
-$host = 'localhost';
-$db = 'login_system';
-$user = 'ultradabkunk';
-$pass = 'test1234';
-$charset = 'utf8mb4';
+$env = parse_ini_file(__DIR__ . '/../.env');
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES => false,
-];
+$conn = new mysqli(
+    $env['DB_HOST'],
+    $env['DB_USER'],
+    $env['DB_PASS'],
+    $env['DB_NAME']
+);
 
-try {
-    $pdo = new PDO($dsn, $user, $pass, $ $options);
-} catch (\PDOException $e) {
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+//check connection
+if(!$conn){
+    echo 'Connection error: ' . mysqli_connect_error();
 }
 
 ?>
