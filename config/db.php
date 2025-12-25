08be91bcee01d0e18628ae87a57147c424065cc9
Ultra Dabkunk <narabodeechaengphrai@gmail.com>
2025-12-25 15:41:13 +0700

    fix connection between .env and index.php

--- config/db.php
+++ config/db.php
@@ -1,20 +1,18 @@
-<?php 
+<?php
 
-$env = parse_ini_file(__DIR__ . '/../.env');
+$env = parse_ini_file(__DIR__ . '/../../.env');
 
-$conn = new PDO(
-    "mysql:host={$env['DB_HOST']};dbname={$env['DB_NAME']};charset=utf8mb4",
-    $env['DB_USER'],
-    $env['DB_PASS'],
-    [
-        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    ]
-);
-
-//check connection
-if(!$conn){
-    echo 'Connection error: ' . mysqli_connect_error();
+try {
+    $conn = new PDO(
+        "mysql:host={$env['DB_HOST']};dbname={$env['DB_NAME']};charset=utf8mb4",
+        $env['DB_USER'],
+        $env['DB_PASS'],
+        [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ]
+    );
+} catch (PDOException $e) {
+    die('Database connection failed: ' . $e->getMessage());
 }
 
-?>
